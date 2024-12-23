cda553f831119bf5fa5686105010e8af09f97940
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-12-23 20:04:52 +0330

    up

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ document.getElementById("modeToggle").addEventListener("click", function() {
         document.getElementById("modeToggle").textContent = "change to light";
     } else {
         themeLink.setAttribute("href", "light-mode.css");
-        document.getElementById("modeToggle").textContent ="change to dark";
+        document.getElementById("modeToggle").textContent = "change to dark"
     }
 });
